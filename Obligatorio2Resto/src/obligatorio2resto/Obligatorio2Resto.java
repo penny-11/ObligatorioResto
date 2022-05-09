9be0f787c93866272824357f79f9cfd64fa35a26
tomas lanterna <tomas.lanterna@hotmail.com>
2022-05-09 15:16:56 -0300

    Creacion de Repo

---
+++ Obligatorio2Resto/src/obligatorio2resto/Obligatorio2Resto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package obligatorio2resto;
+
+/**
+ *
+ * @author tomas
+ */
+public class Obligatorio2Resto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
